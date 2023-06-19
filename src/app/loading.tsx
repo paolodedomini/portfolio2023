6a98687630697db42bcbf7b5de8f060b2f9d3b2f
grafica <paolo@monema.it>
2023-06-19 15:28:16 +0200

    stile pagina loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,20 @@ import React from "react";
 type Props = {};
 
 function Loading({}: Props) {
-  return <h1>Loading...</h1>;
+  return (
+    <main
+      style={{
+        width: "100%",
+        minHeight: 400,
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Loading...</h1>
+    </main>
+  );
 }
 
 export default Loading;
